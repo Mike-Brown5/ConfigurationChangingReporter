26e8bc5cdab797bc097b7b0f4028fd4a45dbe3f7
mike-brown5 <mikeaxe5@outlook.com>
2022-11-22 22:51:32 +0200

    adding a session to connect via ssh

--- App.py
+++ App.py
@@ -3,3 +3,18 @@ from netmiko import ConnectHandler
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+#IP address for the device
+ip = "192.168.104.120"
+
+# Assigning the device type for netmiko custtomization
+device_type = "arista_eos"
+
+user_name= "admin"
+pasword = "admin"
+
+command = "sh ru"
+
+# using SSH session to connect
+session = ConnectHandler(device_type=device_type,ip= ip, username=user_name,password = pasword,global_delay_factor=4)
+
+output = session.send_command(command)
