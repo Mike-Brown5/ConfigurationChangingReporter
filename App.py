7f9147aa879fdf2473600263752e7a11db81f1f8
mike-brown5 <mikeaxe5@outlook.com>
2022-11-23 23:32:09 +0200

    finalizing the project and testing it

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
-import difflib, datetime, smtplib
-from netmiko import ConnectHandler
+import difflib, datetime, smtplib ,ssl
+from netmiko import Netmiko, ConnectHandler
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -15,38 +15,50 @@ pasword = "admin"
 command = "sh ru"
 
 # using SSH session to connect
-session = ConnectHandler(device_type=device_type,ip= ip, username=user_name,password = pasword,global_delay_factor=4)
+print("connecting to the switch.......")
+session = ConnectHandler(device_type=device_type,ip= ip, username=user_name,password = pasword,global_delay_factor=5)
 
+#accesing the enable terrminal
 enable =session.enable()
 
-output = session.send_command(command)
 
-deviceOldConfig = "configFiles/" + "_" + ip + (datetime.date().today() - datetime.timedelta(days=1)).isoformat()
+print("sending commands.......")
+output = session.send_command(command,read_timeout=15)
 
-currentConfig = "configFiles/" + ip + datetime.date().today().isoformat()
 
+deviceOldConfig = "configFiles/" + ip + '_' + (datetime.date.today() - datetime.timedelta(days = 1)).isoformat()
+
+currentConfig = "configFiles/" + ip + datetime.date.today().isoformat()
+print("Writing down the runnig config.......")
 with open(currentConfig,'w') as newConfig:
     newConfig.write(output + "\n")
-
+print("comparing and highliting the diffrent configs.......")
 with open(deviceOldConfig,'r') as oldFile ,open(currentConfig,'r') as newFile:
     diff = difflib.HtmlDiff().make_file(fromlines = oldFile.readlines(), tolines=newFile.readlines(), fromdesc="Yesterday", todesc="Todaay")
 
-fromE = "mike505brown@gmail.com"
-toE = "mike505brown@gmail.com"
 
+
+
+
+print("connecting to protonmail server.......")
+mail = "mikeaxe5@outlook.com"
+pas = "ThisIsMyOutlook"
+
+server = smtplib.SMTP("smtp-mail.outlook.com", port=587)
+server.connect("smtp-mail.outlook.com", port=587)
+server.ehlo()
+server.starttls()
+server.ehlo()
+print("logining in.......")
+server.login(user=mail, password=pas)
+
+
+print("sending the Email.....")
 message = MIMEMultipart()
-message['From'] = fromE
-message['To'] = toE
+message['From'] = mail
+message['To'] = mail
 message['Subject'] = "Daily configration diffrence report"
 
 message.attach(MIMEText(diff,'html'))
-# sending email via gmail server using port 587
-server = smtplib.SMTP('smtp.gmail.com',587)
-
-# encrypting all smtp commands
-server.starttls()
-
-server.login(mike505brown, Mike55555)
-
-server.sendmail(fromE, toE, message.as_string())
+server.sendmail(from_addr=mail, to_addrs=mail ,msg=message.as_string())
 server.quit()
